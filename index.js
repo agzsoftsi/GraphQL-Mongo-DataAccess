15302c9cc63a270fe9867276d3370b8e64f9d6d1
agzsoftsi <agzsoftsi@gmail.com>
2020-05-24 06:54:53 -0500

    Update Index Iintegrate Schema, new Route, GUI for queries on graphQL

--- index.js
+++ index.js
@@ -8,5 +8,26 @@ const express_graphql = require('express-graphql');
 // module to create schemas
 const { buildSchema } = require('graphql');
 
+// build schema
+const schema = buildSchema(`
+type Query{
+    message: String
+}
+
+`);
+
+//
+const root = {
+    message: () => "Hello Wold"
+}
+
+// create route
+app.use('/graphql', express_graphql({
+    schema: schema,
+    root: root,
+    graphiql: true
+}));
+
+
 //create the server
 app.listen(3000, () => console.log('Server on Port 3000'));
