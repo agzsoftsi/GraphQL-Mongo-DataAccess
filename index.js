07b449bde6d079f79a5bb92d8b0518c501d53ba7
agzsoftsi <agzsoftsi@gmail.com>
2020-05-24 06:46:49 -0500

    Update Index - add modules to integrate graphQL and create Schemas

--- index.js
+++ index.js
@@ -2,6 +2,11 @@
 const express = require('express');
 const app = express();
 
+//integrate GraphQL
+const express_graphql = require('express-graphql');
+
+// module to create schemas
+const { buildSchema } = require('graphql');
 
 //create the server
 app.listen(3000, () => console.log('Server on Port 3000'));
